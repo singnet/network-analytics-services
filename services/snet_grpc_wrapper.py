f4d5c78e36e495ea7c40033db92282778fe33756
Eyob Yirdaw <edyirdaw@gmail.com>
2018-09-28 11:08:54 +0300

    done projectedGraph endpoint

--- services/snet_grpc_wrapper.py
+++ services/snet_grpc_wrapper.py
@@ -80,9 +80,59 @@ class NetworkAnalytics(network_analytics_pb2_grpc.NetowrkAnalyticsServicer):
 
     def ProjectedGraph(self,request,context):
 
-        pass
+        print('>>>>>>>>>>>>>>In endpoint ProjectedGraph')
+        print(time.strftime("%c"))
+
+        bipartite_graph = request.graph
+        nodes = request.nodes
+        weight = request.weight
+
+        b = bipartite_graphs.BipartiteGraphs()
+
+        try:
+
+            edges_list = []
 
 
+            for edges_proto in bipartite_graph.edges:
+                edges_list.append(list(edges_proto.edge))
+
+            bipartite_graph_in = {"bipartite_0":list(bipartite_graph.bipartite_0),"bipartite_1":list(bipartite_graph.bipartite_1),"edges":edges_list}
+            nodes_in = {"nodes": list(nodes)}
+
+            ret = b.projected_graph(bipartite_graph_in, nodes_in, weight)
+
+            resp = network_analytics_pb2.ProjecetedGraphResponse(status=ret[0],message=ret[1])
+
+            if resp.status:
+                edges_resp = []
+                for edge_ret in ret[2]["edges"]:
+                    edges_resp.append(network_analytics_pb2.Edge(edge=edge_ret))
+
+                graph_resp = network_analytics_pb2.Graph(nodes=ret[2]["nodes"],edges=edges_resp,weights=ret[2]["weights"])
+
+
+                resp = network_analytics_pb2.ProjecetedGraphResponse(status=ret[0],message=ret[1],output=graph_resp)
+
+
+            print('status:',resp.status)
+            print('message:',resp.message)
+            print('Waiting for next call on port 5000.')
+
+            return resp
+
+
+        except Exception as e:
+
+            logging.exception("message")
+
+            resp = network_analytics_pb2.ProjecetedGraphResponse(status=False,message=str(e))
+
+            print('status:', resp.status)
+            print('message:', resp.message)
+            print('Waiting for next call on port 5000.')
+
+            return resp
 
 
 def serve():
--- services/snet_test_client.py
+++ services/snet_test_client.py
@@ -80,8 +80,8 @@ def test_4():
     # print(type(edge_1.edge))
 
 
-    # graph_in = network_analytics_pb2.BipartiteNodes(bipartite_0=input_0_0["bipartite_0"],bipartite_1=input_0_0["bipartite_1"])
-    graph_in = network_analytics_pb2.BipartiteNodes(bipartite_1=input_0_0["bipartite_1"])
+    graph_in = network_analytics_pb2.BipartiteNodes(bipartite_0=input_0_0["bipartite_0"],bipartite_1=input_0_0["bipartite_1"])
+    # graph_in = network_analytics_pb2.BipartiteNodes(bipartite_1=input_0_0["bipartite_1"])
 
 
     graph_1 = network_analytics_pb2.BipartiteGraphRequest(nodes=graph_in,edges=edges_1)
@@ -93,11 +93,47 @@ def test_4():
     print(response.message)
     print(response.output)
 
+def test_5():
+    channel = grpc.insecure_channel('localhost:5000')
+    stub = network_analytics_pb2_grpc.NetowrkAnalyticsStub(channel)
 
+    input_0_0 = {"bipartite_0": ['8', '7', '6', '10', '12', '13'], "bipartite_1": ['5', '3', '4', '1', '2', '3'],
+                 "edges": [['3', '8'], ['4', '7'], ['5', '6'], ['3', '7']]}
+    input_0_1 = {"nodes":['8', '7']}
+    input_0_2 = 'none'
+
+    # input_0_0 = {"bipartite_0": ['Pam', 'Goeff', 'Philip', 'Sam', 'Fred', 'Jane', 'Sue', 'Charlie'],
+    #              "bipartite_1": ['American Diner', 'Sushi', 'Italian', 'Indian', 'Chinese', 'Tapas', 'Thai', 'French',
+    #                              'Hungarian', 'Lebanese', 'Greek'],
+    #              "edges": [['Pam', 'French'], ['Pam', 'Hungarian'], ['Pam', 'Sushi'], ['Goeff', 'American Diner'],
+    #                        ['Goeff', 'Indian'], ['Goeff', 'Chinese'], ['Philip', 'Lebanese'], ['Philip', 'Italian'],
+    #                        ['Philip', 'Tapas'], ['Sam', 'American Diner'], ['Sam', 'Sushi'], ['Sam', 'Italian'],
+    #                        ['Fred', 'Italian'], ['Fred', 'Tapas'], ['Fred', 'Thai'], ['Jane', 'French'],
+    #                        ['Jane', 'Hungarian'], ['Jane', 'Sushi'], ['Sue', 'Greek'], ['Sue', 'Tapas'],
+    #                        ['Sue', 'Thai'], ['Charlie', 'American Diner'], ['Charlie', 'Indian'],
+    #                        ['Charlie', 'Chinese']]}
+    # input_0_1 = {"nodes": ['Pam', 'Charlie', 'Goeff', 'Fred', 'Sam', 'Sue', 'Philip', 'Jane']}
+    # input_0_2 = 'degree'
+
+    edges_req = []
+    for e in input_0_0["edges"]:
+        edges_req.append(network_analytics_pb2.Edge(edge=e))
+
+
+    graph_in = network_analytics_pb2.BipartiteGraph(bipartite_0=input_0_0["bipartite_0"],bipartite_1=input_0_0["bipartite_1"],edges=edges_req)
+
+
+    graph_1 = network_analytics_pb2.ProjecetedGraphRequest(graph=graph_in,nodes=input_0_1["nodes"],weight=input_0_2)
+
+    response = stub.ProjectedGraph(graph_1)
+    print(response.status)
+    print(response.message)
+    print(response.output)
 
 if __name__ == '__main__':
 
-    test_4()
+    test_5()
+    # test_4()
     # test_1()
     # print('Using test_2')
     # time.sleep(2)
