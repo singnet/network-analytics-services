a58b0fa88de88f26983590e3fb369c22338176c6
Eyob Yirdaw <edyirdaw@gmail.com>
2018-11-23 11:03:39 +0300

    skeleton for graphs services

---
+++ services/graphs.py
@@ -0,0 +1,51 @@
+# Tested on python3.6
+
+
+
+class Graphs:
+
+    def __init__(self):
+
+
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__end__ = '__end__'
+
+
+if __name__ == '__main__':
+
+    pass
---
+++ services/test_graphs.py
@@ -0,0 +1,56 @@
+# Tested on python3.6
+
+import unittest
+import graphs
+
+
+
+
+
+class TestGraphs(unittest.TestCase):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+__end__ = '__end__'
+
+if __name__ == '__main__':
+    unittest.main()
