72c5dbe93e11e98f003758e646acf453073c43aa
Tensae <tensaeberhan@ymail.com>
2019-01-05 18:54:17 +0300

    removed call credentials from the grpc wrapper

--- services/graph_services/test_snet_grpc_wrapper_util.py
+++ services/graph_services/test_snet_grpc_wrapper_util.py
@@ -70,16 +70,6 @@ def test_2():#Check MostImportantNodes
     print(response.edge_betweenness_centrality)
 
 
-def generate_call_credentials():
-    agent_address = "0xb57B4c70379E84CD8d42a867cF326d5e0743E11d"  # NetworkAnalyticsServices deployed to Kovan
-    result = subprocess.check_output(["snet", "agent", "--at", agent_address, "create-jobs", "--funded", "--signed", "--no-confirm", "--max-price","3"])
-    job = yaml.load(result)["jobs"][0]
-    job_address = job["job_address"]
-    job_signature = job["job_signature"]
-    print("job_address------------------------------")
-    print(job_address)
-    print("job_signature----------------------------")
-    print(job_signature)
 
 
 if __name__ == '__main__':
